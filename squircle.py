aa9fcefc7ff706f84c500bdf22916f68a20da795
christiangynnild <75206913+ChristianGynnild@users.noreply.github.com>
2025-11-06 16:41:07 +0100

    Add width nm to squircle

--- squircle.py
+++ squircle.py
@@ -5,6 +5,12 @@ import numpy as np
 from dataclasses import dataclass
 import json
 
+def lerp(a,b,x):
+    return a*(1-x) + b*(x)
+
+size_of_picture = 40
+
+
 @dataclass
 class Squircle:
     figure_name: str
@@ -12,6 +18,7 @@ class Squircle:
     squircle_factor: int
     data: np.ndarray
     pixel_size:float
+    width_nm:int
 
     def serialize(self, filename:str):
         squircle_dict = asdict(self)
@@ -23,6 +30,38 @@ class Squircle:
         
         with open(str(filename_path.with_suffix(".npy")), "wb") as file:
             np.save(file, self.data, allow_pickle=False)
+
+    @property
+    def width_pixels(self):
+        return self.width_nm/self.pixel_size
+
+    def get_squircle_mask(self):
+        """
+        Produces an ellipse. Radius changes with N as radius =  N+7 and rotation as rotation=k*10 degrees
+        """
+        # Count is number of figures from square to circle
+        size = self.width_pixels
+        radius = lerp(0, size, (self.squircle_factor)/(5))
+        x = np.arange(-int(size_of_picture/2),int(size_of_picture/2))
+        y = np.arange(-int(size_of_picture/2),int(size_of_picture/2))
+        xx,yy = np.meshgrid(x,y,indexing='ij')
+
+        cross_width = size - radius
+        circle_x_position = abs(size) - radius
+        circle_y_position = abs(size) - radius
+
+        pixels = np.logical_or(
+            np.logical_and((xx**2) <= size**2, (yy**2) <= cross_width**2),
+            np.logical_or(
+                np.logical_and((yy**2) <= size**2, (xx**2) <= cross_width**2),
+                (np.abs(xx)-circle_x_position)**2 + (np.abs(yy) - circle_y_position)**2 <= radius**2
+            )
+        )
+        return pixels
+    
+    def get_masked_data(self):
+        pass
+
 
 def deserialize(filename):
     filename_path = Path(filename)
